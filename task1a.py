915f3846e4f0e4027b3b80a8b3d9a0e8792c1680
Jayesh1218 <jy331399@dal.ca>
2019-02-15 11:09:52 -0800

    task1.1

--- task1a.py
+++ task1a.py
@@ -1,21 +1,11 @@
- #program stated
-
-fin = open ("Book1.txt",r)
-fin = fin.read()
-fin = fin.split()
-
-def unique_words(book):
-    unique_words = []
-    for word in unique_words:
-    fin = line.lower().strip(string.punctuation + string.whitespace)
-
-         total = {}
-    for word in  str1:
-         total[word] = total.get(word, 0) + 1
-
-  print("Total Words : " + str(unique_words_it))
-  
-
-
-print(unique_words())
-
+file1=open("Book1.txt")
+file2=open("Book2.txt")
+file3=open("Book3.txt")
+def unique_words(file):
+	l=[]
+	for line in file:
+		line=line.split()
+		if line not in l:
+			l.append(line)
+	return l
+print(unique_words(file1))
