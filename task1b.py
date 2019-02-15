f0340d072a1bde48ff3375c7179dd4a2af997050
Jayesh1218 <jy331399@dal.ca>
2019-02-15 11:20:26 -0800

    task performed

--- task1b.py
+++ task1b.py
@@ -1,2 +1,14 @@
-import string 
-string =string.punctuation
+filename=open("Book2.txt")
+def process_file(filename):
+	hist = dict()
+	for line in filename:
+		most_common(hist)
+	return hist
+def most_common(hist):
+	t = []
+	for key, value in hist.items():
+		t.append((value, key))
+		t.sort(reverse=True)
+	return t
+
+hist = process_file('Book2.txt')
