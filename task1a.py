4ff26a8220ea3403598d7b0fe6e511de7202448f
Jayesh1218 <jy331399@dal.ca>
2019-02-15 10:36:53 -0800

    task1a

--- task1a.py
+++ task1a.py
@@ -1,4 +1,4 @@
-#program stated
+ #program stated
 
 fin = open ("Book1.txt",r)
 fin = fin.read()
@@ -6,5 +6,16 @@ fin = fin.split()
 
 def unique_words(book):
     unique_words = []
-for word in unique_words:
-    
+    for word in unique_words:
+    fin = line.lower().strip(string.punctuation + string.whitespace)
+
+         total = {}
+    for word in  str1:
+         total[word] = total.get(word, 0) + 1
+
+  print("Total Words : " + str(unique_words_it))
+  
+
+
+print(unique_words())
+
